9daa5a3b179b98bef60b5586290ebba76c90d614
tinug <tinug@192.168.0.17>
2019-06-13 13:46:29 -0700

    Test commit1

---
+++ Demo/src/demo/Hello.java
@@ -0,0 +1,24 @@
+
+package demo;
+
+import java.util.Scanner;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String abc;
+		
+		System.out.println("Enter a number");
+		
+		Scanner obj = new Scanner(System.in);
+		
+		abc= obj.next();
+		
+		System .out.println(abc);
+		
+		
+	}
+
+}
